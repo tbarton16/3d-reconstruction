d87822e7a9c6465d26bf5ecb5a2a939a7e913b35
Theresa Barton <tbarton@cs227-29.local>
2015-11-23 21:40:41 -0800

    still havent figured out normalization

--- just in case.m
+++ just in case.m
@@ -7,3 +7,9 @@
         save([name1 ' 2dpts'],'movingPoints');
         save([name2 ' 2dpts'], 'fixedPoints');
         save([name1 name2 ' 3d'], 'real_points');
+        
+        
+        
+    load('/home/tbarton/Desktop/Project4/DSCF4178 2dpts.mat')
+    load('/home/tbarton/Desktop/Project4/DSCF4177 2dpts.mat')
+    load('/home/tbarton/Desktop/Project4/DSCF4177DSCF4178.mat')
